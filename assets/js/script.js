274b112dc42433e06244f322f6c77da08686aba8
Arllan Pablo <arllanpablo@gmail.com>
2020-02-08 13:13:51 -0300

    adicionado site

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+    var tela = $(document).height();
+    console.log(tela);
+    $("body").css({"height":+tela,"background-image":"linear-gradient(to top, rgb(116,237,250), rgb(66,101,254))"});
+ 
